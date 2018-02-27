ed11d7b0ff775c8df10e51e070e43c3bfb520e3e
sklaw <sk.law.lsq@gmail.com>
2018-02-27 20:23:10 +0800

    Implementing

--- scripts/alvar_marker_to_baxter_picking_pose.py
+++ scripts/alvar_marker_to_baxter_picking_pose.py
@@ -1,7 +1,116 @@
 #!/usr/bin/env python
 import rospy
+from ar_track_alvar_msgs.msg import AlvarMarkers
+import tf
+from tf.transformations import (
+    translation_matrix,
+    quaternion_matrix,
+    translation_from_matrix,
+    quaternion_from_matrix,
+)
+import threading
+import copy
+import numpy
+import ipdb
+
+handcoded_marker_compensation = {
+    "0": numpy.array((
+        (1.0, 0.0, 0.0, 0.0),
+        (0.0, 1.0, 0.0, 0.0),
+        (0.0, 0.0, 1.0, 0.0),
+        (0.0, 0.0, 0.0, 1.0)
+    ), dtype=numpy.float64),
+    "8": numpy.array((
+        (1.0, 0.0, 0.0, 0.0),
+        (0.0, 1.0, 0.0, 0.0),
+        (0.0, 0.0, 1.0, 0.0),
+        (0.0, 0.0, 0.0, 1.0)
+    ), dtype=numpy.float64),
+    "13": numpy.array((
+        (1.0, 0.0, 0.0, 0.0),
+        (0.0, 1.0, 0.0, 0.0),
+        (0.0, 0.0, 1.0, 0.0),
+        (0.0, 0.0, 0.0, 1.0)
+    ), dtype=numpy.float64),
+}
+
+writable = threading.Event()
+writable.clear()
+shared_msg = None
+
+def cb(msg):
+    global writable, shared_msg
+    if writable.is_set():
+        shared_msg = msg
+
+def transform_into_baxter_picking_space(mat):
+    for axis in range(3):
+        swap_with =  abs(mat[:3, axis]).argmax()
+        if swap_with != axis:
+            tmp = mat[:3, swap_with].copy()
+            mat[:3, swap_with] = mat[:3, axis]
+            mat[:3, axis] = tmp
+    
+    if mat[0][0] > 0:
+        mat[:3, 0] = -mat[:3, 0]
+    if mat[1][1] < 0:
+        mat[:3, 1] = -mat[:3, 1]
+    if mat[2][2] > 0:
+        mat[:3, 2] = -mat[:3, 2]
+
+    return mat
+        
 
 if __name__ == '__main__':
     rospy.init_node("alvar_marker_to_baxter_picking_pose_py")
-    rospy.logerr("TODO")
     
+    rospy.Subscriber("ar_pose_marker", AlvarMarkers, cb)
+
+    writable.set()
+
+    listener = tf.TransformListener()
+
+    look_up_t = rospy.Time(0)
+    listener.waitForTransform('base', 'camera_link', look_up_t, rospy.Duration(3))
+    base_to_cam = listener.lookupTransform('base', 'camera_link', look_up_t)
+    base_to_cam_mat = listener.fromTranslationRotation(*base_to_cam) 
+
+    broadcaster = tf.TransformBroadcaster()
+
+    r = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        writable.clear()
+        msg = copy.deepcopy(shared_msg)
+        writable.set()
+
+        if msg is not None:
+            for marker in msg.markers:
+                pose = marker.pose.pose
+                pos = pose.position
+                ori = pose.orientation
+                cam_to_marker_mat = numpy.dot(translation_matrix((pos.x, pos.y, pos.z)), quaternion_matrix((ori.x, ori.y, ori.z, ori.w)))
+                base_to_marker = numpy.dot(base_to_cam_mat, cam_to_marker_mat)
+                
+                broadcaster.sendTransform(
+                    translation_from_matrix(base_to_marker),
+                    quaternion_from_matrix(base_to_marker),
+                    rospy.Time.now(),
+                    'raw_marker_%s'%marker.id,
+                    'base', 
+                )
+                
+                if marker.id in handcoded_marker_compensation:
+                    base_to_marker = numpy.dot(base_to_marker, handcoded_marker_compensation[marker.id])
+                base_to_marker = transform_into_baxter_picking_space(base_to_marker) 
+                broadcaster.sendTransform(
+                    translation_from_matrix(base_to_marker),
+                    quaternion_from_matrix(base_to_marker),
+                    rospy.Time.now(),
+                    'baxter_picking_pose_%s'%marker.id,
+                    'base', 
+                )
+
+        try:
+            r.sleep()
+        except rospy.ROSInterruptException:
+            break
