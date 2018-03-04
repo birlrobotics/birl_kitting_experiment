daa40d371cc02943f19ed5311a043b3d653e7c42
sklaw <sk.law.lsq@gmail.com>
2018-03-04 16:41:48 +0800

    new DMP models

--- src/birl_kitting_experiment/hardcoded_data.py
+++ src/birl_kitting_experiment/hardcoded_data.py
@@ -2,24 +2,12 @@ from geometry_msgs.msg import (
     Pose,
     Quaternion,
 )
-home_pose = Pose()
-home_pose.position.x = 0.676797102353
-home_pose.position.y = -0.537927061899
-home_pose.position.z = 0.211797607526
-home_pose.orientation = Quaternion(
-    x= 0.999853989551,
-    y= -0.0105520248441,
-    z= 0.00428238956375,
-    w= 0.0127403096533,
-)
 
 place_pose = Pose()
-place_pose.position.x = 0.0689814878187
-place_pose.position.y = -0.809844772878
-place_pose.position.z = 0.110868228768
-place_pose.orientation = Quaternion(
-    x= 0.00602278560434,
-    y= 0.999630513296,
-    z= -0.0262524593887,
-    w= 0.00365668116077,
-)
+place_pose.position.x = 0.526379460142
+place_pose.position.y = -0.818221808593
+place_pose.position.z = 0.0342286718843
+place_pose.orientation.x= 0.999937797296
+place_pose.orientation.y= -0.0043522273979
+place_pose.orientation.z= -0.00938816830392
+place_pose.orientation.w= 0.00416196480575
